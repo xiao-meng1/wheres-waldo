7c7363013aa9f965e670e3435f9e3429cffcadcd
Xiao <xmeng1@ualberta.ca>
2022-06-22 20:30:16 -0600

    Set up firebase defaults

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJq58SHe_5G16tXb1RjSfqGlCSTcrAyHc',
+  authDomain: 'whereswaldo-a4a4a.firebaseapp.com',
+  projectId: 'whereswaldo-a4a4a',
+  storageBucket: 'whereswaldo-a4a4a.appspot.com',
+  messagingSenderId: '887062179868',
+  appId: '1:887062179868:web:0e35b83db2eeda5f4a2fd9',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
