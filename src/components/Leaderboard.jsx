de38dc6820567b5f54125f74795e40e34b3c1958
Xiao <xmeng1@ualberta.ca>
2022-06-24 21:20:49 -0600

    Add static Leaderboard page

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styles from '../styles/leaderboard.module.css';
 
 function Leaderboard() {
-  return <div />;
+  return (
+    <section className={styles.container}>
+      <h2>Leaderboard</h2>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Time &#40;seconds&#41;</th>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>93.2</td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>93.2</td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>93.2</td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>93.2</td>
+        </tr>
+      </table>
+    </section>
+  );
 }
 
 export default Leaderboard;
