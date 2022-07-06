9568842e407a5feebeb0d24c5da2f8e515ea3c66
Xiao <xmeng1@ualberta.ca>
2022-07-05 20:35:03 -0600

    Update styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,48 +5,50 @@ import waldoIMG from '../assets/images/Waldo.jpeg';
 
 function Header() {
   return (
-    <header className={styles.header} role="navigation">
-      <Link to="/" className={styles.left}>
-        <img src={waldoIMG} alt="Waldo" />
-        <h1>
-          <span className={styles.blue}>Where&apos;s </span>
-          <span className={styles.red}>Waldo?</span>
-        </h1>
-      </Link>
-      <nav className={styles.right}>
-        <ul>
-          <li key="Home">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? styles.active : undefined
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li key="Play">
-            <NavLink
-              to="play"
-              className={({ isActive }) =>
-                isActive ? styles.active : undefined
-              }
-            >
-              Play
-            </NavLink>
-          </li>
-          <li key="Leaderboard">
-            <NavLink
-              to="leaderboard"
-              className={({ isActive }) =>
-                isActive ? styles.active : undefined
-              }
-            >
-              Leaderboard
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <Link to="/" className={styles.left}>
+          <img src={waldoIMG} alt="Waldo" />
+          <h1>
+            <span className={styles.blue}>Where&apos;s </span>
+            <span className={styles.red}>Waldo?</span>
+          </h1>
+        </Link>
+        <nav className={styles.right}>
+          <ul>
+            <li key="Home">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? styles.active : undefined
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li key="Play">
+              <NavLink
+                to="play"
+                className={({ isActive }) =>
+                  isActive ? styles.active : undefined
+                }
+              >
+                Play
+              </NavLink>
+            </li>
+            <li key="Leaderboard">
+              <NavLink
+                to="leaderboard"
+                className={({ isActive }) =>
+                  isActive ? styles.active : undefined
+                }
+              >
+                Leaderboard
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
